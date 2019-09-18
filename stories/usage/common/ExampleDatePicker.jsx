5127dd494c4f3b4849be9ca980e2cc490e1d0a80
Mitchell Hillman <mitchell.hillman@bottomline.com>
2019-09-18 14:43:10 -0400

    Fix a11y violation in storybook

--- stories/usage/common/ExampleDatePicker.jsx
+++ stories/usage/common/ExampleDatePicker.jsx
@@ -1,4 +1,5 @@
 /* eslint-disable react/prop-types */
+/* eslint-disable jsx-a11y/label-has-associated-control */
 
 import React from 'react';
 import { action } from '@storybook/addon-actions/dist/index';
@@ -6,17 +7,20 @@ import { DatePicker } from '../../../src';
 import './custom-classes.css';
 
 const defaultProps = {
-  htmlId: 'slaanesh',
-  name: 'khorne',
+  htmlId: 'foobar',
+  name: 'foobaz',
   onChangeCallback: action('selection changed'),
   rangeDelimiter: '–'
 };
 
 const DatePickerExample = (props) => (
-  <DatePicker
-    {...defaultProps}
-    {...props}
-  />
+  <label>
+    <span style={{ 'font-family': 'sans-serif', margin: '10px 0', display: 'block' }}>Datepicker</span>
+    <DatePicker
+      {...defaultProps}
+      {...props}
+    />
+  </label>
 );
 
 export default DatePickerExample;
